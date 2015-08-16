eeb9f93c209930ff6ec708c8070a3823c5cb82b6
Marcelo Diniz <marudiniz@gmail.com>
2015-08-16 23:39:18 +0200

    Add descriptor to PaymentRequest Response

--- src/main/Response/PaymentRequest.php
+++ src/main/Response/PaymentRequest.php
@@ -4,8 +4,19 @@ namespace RatePay\Response;
 
 class PaymentRequest extends Base {
 
+  protected $_descriptor;
+
+  public function parseResponse() {
+    parent::parseResponse();
+    $this->_descriptor = $this->_root->content->payment->descriptor;
+  }
+
   public function isApproved() {
     return $this->_responseType == 'PAYMENT_PERMISSION';
+  }
+
+  public function getDescriptor() {
+    return $this->_descriptor;
   }
 
 }
